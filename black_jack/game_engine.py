067e952bc8bdf44ac98c3c0e000e3ae42ffa2f92
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-27 22:14:33 +0300

    lint fix

--- black_jack/game_engine.py
+++ black_jack/game_engine.py
@@ -12,44 +12,43 @@ def run_game():
         return
     if bet == 'quit':
         return print('See you maybee next time!\nGoodbye!')
-    while int(balance) > 0 and choise != 'quit' and bet != 'quit':  
+    while int(balance) > 0 and choise != 'quit' and bet != 'quit':
         numbers = [6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
         cards_value = (6, 7, 8, 9, 10, 10, 10, 10, 11)
         if bet == 'quit':
-            break  
+            break
         dil_num1, dil_simb1, dil_num2, dil_simb2 = make_cards.make_dill_cards()
-        curent_diller_value = int(cards_value[numbers.index(dil_num1)]) + int(cards_value[numbers.index(dil_num2)])
-
+        curent_diller_value = (int(cards_value[numbers.index(dil_num1)])
+                               + int(cards_value[numbers.index(dil_num2)]))
         print('Diller: ???')
         make_cards.print_deal_secret_cards(dil_num1, dil_simb1)
-            
-
         pl_num1, pl_simb1, pl_num2, pl_simb2 = make_cards.make_player_cards()
-        curent_player_value = int(cards_value[numbers.index(pl_num1)]) + int(cards_value[numbers.index(pl_num2)])
-
+        curent_player_value = (int(cards_value[numbers.index(pl_num1)])
+                               + int(cards_value[numbers.index(pl_num2)]))
         print('Player: ' + str(curent_player_value))
         make_cards.print_player_cards(pl_num1, pl_simb1, pl_num2, pl_simb2)
-
         choise = input('Your decision: (more, stop, leave)\n:')
-        
         if choise == 'more':
             pl_num3, pl_simb3 = make_cards.make_player_third_card()
-            curent_player_value = (int(cards_value[numbers.index(pl_num1)]) +
-                                   int(cards_value[numbers.index(pl_num2)]) + 
-                                   int(cards_value[numbers.index(pl_num3)]))
+            curent_player_value = (int(cards_value[numbers.index(pl_num1)])
+                                   + int(cards_value[numbers.index(pl_num2)])
+                                   + int(cards_value[numbers.index(pl_num3)]))
             print('Player: ' + str(curent_player_value))
-            make_cards.print_player_three_cards(pl_num1, pl_simb1, pl_num2, pl_simb2, pl_num3, pl_simb3)
+            make_cards.print_player_three_cards(pl_num1, pl_simb1,
+                                                pl_num2, pl_simb2,
+                                                pl_num3, pl_simb3)
             choise = input('Your decision: (stop, leave)\n:')
-        
         if choise == 'stop':
             print('Diller: ' + str(curent_diller_value))
             make_cards.print_dil_cards(dil_num1, dil_simb1, dil_num2, dil_simb2)
-            balance = process_function.determine_winner(curent_player_value, curent_diller_value, balance, bet)
+            balance = process_function.determine_winner(curent_player_value,
+                                                        curent_diller_value,
+                                                        balance, bet)
             if balance < 1:
                 print("You've lost all the money.")
                 print('Better luck next time!\nThank you and goodbye!')
                 break
-            choise, bet =  process_function.answer_to_play_again(balance)
+            choise, bet = process_function.answer_to_play_again(balance)
         if choise.lower() == 'leave':
             print('Better luck next time!\nThank you and goodbye!')
             break
