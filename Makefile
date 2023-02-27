a4ceaf43c72427b804fc16d896301d4e4148f8cc
Vadim Khamidullin <Khamidullin.VD@bk.ru>
2023-02-27 17:47:57 +0300

    create projeckt

---
+++ Makefile
@@ -0,0 +1,27 @@
+# Resolves the dependencies and installs them
+install: 
+	poetry install
+# Play brain-games
+brain-games:
+	poetry run brain-games
+
+# Make package
+build: 
+	poetry build
+
+# Publish package 
+publish:
+	poetry publish --dry-run
+
+# Install local package 
+package-install: 
+	python3 -m pip install --user dist/*.whl
+
+# Reinstall local package
+package-reinstall:
+	python3 -m pip install --force-reinstall  --user dist/*.whl
+
+# Run lint
+lint: 
+	poetry run flake8 brain_games
+	
---
+++ black_jack/games/brain_progression.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+RULES = 'What number is missing in the progression?'
+
+
+def generate_round():
+    length_progression = randint(5, 10)
+    start = randint(1, 10)
+    step = randint(1, 10)
+    secret_number = randint(0, length_progression - 1)
+    question = ' '
+    progression = [f'{start}', f'{start + step}', f'{start + step * 2}',
+                   f'{start + step * 3}', f'{start + step * 4}',
+                   f'{start + step * 5}', f'{start + step * 6}',
+                   f'{start + step * 7}', f'{start + step * 8}',
+                   f'{start + step * 9}']
+    progression[secret_number] = '..'
+    question = question.join(progression)
+    correct_answer = str(start + (step * secret_number))
+    return question, correct_answer
